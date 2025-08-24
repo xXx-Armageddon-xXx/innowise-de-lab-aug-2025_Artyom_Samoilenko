c5212d3475a5f4115d7d19f01e11e2f1b68c5e1a
xXx-Armageddon-xXx <koliuchka2006@gmail.com>
2025-08-24 16:42:26 +0300

    Update comments in Ex-6.py

--- Ex-6.py
+++ Ex-6.py
@@ -4,30 +4,30 @@ print("    2. Если хотите очистить очистить вычис
 
 operation = None # Создаем пустую переменную, чтобы начать внешний цикл
 
-while operation != 'x': #При вводе 'x' вычисления заканчиваются
+while operation != 'x': # При вводе 'x' вычисления заканчиваются
     
     current_number = float(input("\nВведите первое число: "))
 
-    operation = None #Это нужно для того, чтобы при очистке калькулятора путем ввода 'c', мы могли продолжить вычисления
+    operation = None # Это нужно для того, чтобы при очистке калькулятора путем ввода 'c', мы могли продолжить вычисления
     
     while operation != 'c' and operation != 'x':
 
         operation = input("\nВведите операцию(+,-,*,/,x,c): ")
 
         if operation != 'x' and operation != 'c':
-            second_number = float(input("\nВведите второе число: "))
+            second_number = float(input("\nВведите второе число: ")) # Это нужно, чтобы при вводе 'x' или 'c' калькулятор не просил ввести второе число, т.к это не имеет смысла
         
         if operation == '+':
-            current_number += second_number # сложение
+            current_number += second_number # Сложение
             
         elif operation == '-':
-            current_number -= second_number # вычитание
+            current_number -= second_number # Вычитание
             
         elif operation == '*':
-            current_number *= second_number # произведение
+            current_number *= second_number # Произведение
             
         elif operation == '/':
-            current_number /= second_number # деление
+            current_number /= second_number # Деление
             
         elif operation != 'x' and operation != 'c':
             print("\nПохоже вы выбрали неверное действие! Попробуйте снова.") # Пользователь ввел неверный символ
